3953fba768e3fba337a5f8cad3677164d1fa5d7e
Vivekanand Pujar <vivek@vivekanand.dev>
2026-06-16 17:15:59 +0530

    init

---
+++ src/main/java/com/example/gha_lab/GhaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.gha_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gha_lab/GhaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gha_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
